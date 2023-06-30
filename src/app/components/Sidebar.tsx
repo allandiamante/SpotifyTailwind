d0b6b759e02e483b37a5c0e4f93a880e282af519
unknown <allandiamantedesouza@gmail.com>
2023-06-30 04:04:12 -0300

    refinamento e build 2

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -3,33 +3,33 @@ import { HomeIcon, Library, Search } from "lucide-react";
 export function Sidebar(){
     return(
         <aside className="w-72 bg-zinc-950 p-6">
-          <div className="flex items-center gap-2">
-            <div className="w-3 h-3 bg-red-500 rounded-full "/>
-            <div className="w-3 h-3 bg-yellow-500 rounded-full "/>
-            <div className="w-3 h-3 bg-green-500 rounded-full "/>
+          <div className="flex items-center gap-2 ">
+            <div className="w-3 h-3 bg-red-500 rounded-full hover:bg-red-400"/>
+            <div className="w-3 h-3 bg-yellow-500 rounded-full hover:bg-yellow-400"/>
+            <div className="w-3 h-3 bg-green-500 rounded-full hover:bg-green-400"/>
             
           </div>
           <nav className="space-y-5 mt-10">
-            <a href="" className="flex items-center gap-3 text-sm font-semibolt text-zinc-200">
+            <a href="" className="flex items-center gap-3 text-sm font-semibolt text-zinc-200 hover:text-zinc-400">
               Home
               <HomeIcon/>
             </a>
-            <a href="" className="flex items-center gap-2 text-sm font-semibolt text-zinc-200">
+            <a href="" className="flex items-center gap-2 text-sm font-semibolt text-zinc-200 hover:text-zinc-400">
               Search
               <Search/>
             </a>
-            <a href="" className="flex items-center gap-2 text-sm font-semibolt text-zinc-200">
+            <a href="" className="flex items-center gap-2 text-sm font-semibolt text-zinc-200 hover:text-zinc-400">
               Your Library
               <Library/>
             </a>
           </nav>
           <nav className="mt-10 pt-6 border-t border-zinc-800 flex flex-col gap-3">
-            <a className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 1 </a>
-            <a className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 2</a>
-            <a className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 3</a>
-            <a className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 4</a>
-            <a className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 5</a>
-            <a className="text-sm text-zinc-400 hover:text-zinc-100">Playlist 1</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100">Poison Heart</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100">Another Brick In The Wall</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100">Hurt</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100">Fear Of The Dark</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100">Somebody To Love</a>
+            <a className="text-sm text-zinc-400 hover:text-zinc-100">Folson Prison Blues</a>
           </nav>
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
         <Sidebar />
         <main className="flex-1 p-6">
           <div className="flex items-center gap-4">
-            <button className="rounded-full bg-black/40 p-4">
+            <button className="rounded-full bg-black/40 p-4 hover:text-zinc-500">
               <ChevronLeft />
             </button>
-            <button className="rounded-full bg-black/40 p-4">
+            <button className="rounded-full bg-black/40 p-4 hover:text-zinc-500">
               <ChevronRight />
             </button>
           </div>
           <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
           <div className="grid grid-cols-3 gap-4 mt-4">
-            <a href="#" className="bg-white/10  group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+          <a href="#" className="bg-white/10 group rounded flex items-center gap-4 hover:bg-white/20 transition-colors">
               <Image src="/tomcapa.jpg" width={104} height={104} alt=" capa da banda x" />
               <strong> As melhores de Tom Zé</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
@@ -28,35 +28,35 @@ export default function Home() {
             </a>
             <a href="#" className="bg-white/10 group rounded flex items-center gap-4 hover:bg-white/20 transition-colors">
               <Image src="/pinkcapa.png" width={104} height={104} alt=" capa da banda x" />
-              <strong> As melhores de PinkFloyd</strong>
+              <strong> The Division</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
             </a>
             <a href="#" className="bg-white/10 group rounded flex items-center gap-4 hover:bg-white/20 transition-colors">
-              <Image src="/tomcapa.jpg" width={104} height={104} alt=" capa da banda x" />
-              <strong> As melhores de Tom Zé</strong>
+              <Image src="/queencapa.jpg" width={104} height={104} alt=" capa da banda x" />
+              <strong> Queen II</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
             </a>
             <a href="#" className="bg-white/10 group rounded flex items-center gap-4 hover:bg-white/20 transition-colors">
-              <Image src="/pinkcapa.png" width={104} height={104} alt=" capa da banda x" />
-              <strong> As melhores de PinkFloyd</strong>
+              <Image src="/cashcapa.webp" width={104} height={104} alt=" capa da banda x" />
+              <strong> Johnny Cash American IV: The Man Comes Around</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
             </a>
             <a href="#" className="bg-white/10 group rounded flex items-center gap-4 hover:bg-white/20 transition-colors">
-              <Image src="/tomcapa.jpg" width={104} height={104} alt=" capa da banda x" />
-              <strong> As melhores de Tom Zé</strong>
+              <Image src="/teatrocapa.jpg" width={104} height={104} alt=" capa da banda x" />
+              <strong> A Sociedade Do Espetáculo</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
             </a>
             <a href="#" className="bg-white/10 group rounded flex items-center gap-4 hover:bg-white/20 transition-colors">
-              <Image src="/pinkcapa.png" width={104} height={104} alt=" capa da banda x" />
-              <strong> As melhores de PinkFloyd</strong>
+              <Image src="/vancapa.webp" width={104} height={104} alt=" capa da banda x" />
+              <strong> 1984 - Van Halen</strong>
               <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
                 <Play />
               </button>
@@ -65,29 +65,29 @@ export default function Home() {
           <h2 className="font-semibold text-2xl mt-10">Made for Allan Diamante</h2>
           <div className="grid grid-cols-8 gap-4 mt-4">
             <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col hover:bg-white/10 ">
-              <Image src="/pinkcapa.png" className=" w-full" width={104} height={104} alt=" capa da banda x" />
-              <strong>Daily Mix 1</strong>
-              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
+              <Image src="/summer.jpg" className=" w-full" width={104} height={104} alt=" capa da banda x" />
+              <strong>Summer Eletrohits 2020</strong>
+              <span className="text-sm text-zinc-400">MOJJO, Bhaskar, Vitorio and more</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col hover:bg-white/10 ">
+              <Image src="/queencapa.jpg" className=" w-full" width={104} height={104} alt=" capa da banda x" />
+              <strong>Nevermore</strong>
+              <span className="text-sm text-zinc-400">Brian May, Freddie Mercury, John Deacon e Roger Taylor</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col hover:bg-white/10 ">
+              <Image src="/tomcapa.jpg" className=" w-full" width={104} height={104} alt=" capa da banda x" />
+              <strong>Xique Xique</strong>
+              <span className="text-sm text-zinc-400">Tom zé</span>
+            </a>
+            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col hover:bg-white/10 ">
+              <Image src="/teatrocapa.jpg" className=" w-full" width={104} height={104} alt=" capa da banda x" />
+              <strong>O Anjo Mais Velho</strong>
+              <span className="text-sm text-zinc-400">	Rafael dos Santos, Maria Fernanda e Emerson Marciano</span>
             </a>
             <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col hover:bg-white/10 ">
               <Image src="/pinkcapa.png" className=" w-full" width={104} height={104} alt=" capa da banda x" />
-              <strong>Daily Mix 1</strong>
-              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
-            </a>
-            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col hover:bg-white/10 ">
-              <Image src="/pinkcapa.png" className=" w-full" width={104} height={104} alt=" capa da banda x" />
-              <strong>Daily Mix 1</strong>
-              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
-            </a>
-            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col hover:bg-white/10 ">
-              <Image src="/pinkcapa.png" className=" w-full" width={104} height={104} alt=" capa da banda x" />
-              <strong>Daily Mix 1</strong>
-              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
-            </a>
-            <a href="#" className="bg-white/5 p-3 rounded-md flex flex-col hover:bg-white/10 ">
-              <Image src="/pinkcapa.png" className=" w-full" width={104} height={104} alt=" capa da banda x" />
-              <strong>Daily Mix 1</strong>
-              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
+              <strong>Keep Talking</strong>
+              <span className="text-sm text-zinc-400">Nick Mason, Roger Waters, Bob Klose, Richard Wright ...</span>
             </a>
           </div>
         </main>
