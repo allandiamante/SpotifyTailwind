7d55aae46047bf3376665cb71abfac01900d7860
unknown <allandiamantedesouza@gmail.com>
2023-06-30 03:17:38 -0300

    Deploy projeto do Spotify

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Dashboard(){
+    return(
+        <div>
+            <h1>Dashboard</h1>
+            <Link href="/">Home</Link>
+        </div>
+    )
+}
